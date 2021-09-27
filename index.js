2fa290609d4a1291d5e27f97a5f3c24281ddd839
rheien <anhel_92@hotmail.de>
2021-09-27 15:06:00 +0200

    style.css hochgeladen, jquery und co

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hi from Console')
+const jquery=require('jquery')
+//
+$=window.$=window.jQuery=jquery;
+
+$(function(){
+    let endpoint='https://jsonplaceholder.typicode.com/todos/1'
+    $.ajax({
+        url:endpoint,
+        contentType:"application/json",
+        dataType:'json',
+        success: function(result){
+            console.log(result)
+            var id=document.getElementById("todoID")
+            var title=document.getElementById("todoTitle")
+            var isCompleted=document.getElementById("isC")
+            id.innerHTML='ID: '+result.id
+            title.innerHTML='Title: '+result.title
+            isCompleted.innerHTML='is Completed: '+result.completed
+        }
+    })
+})
